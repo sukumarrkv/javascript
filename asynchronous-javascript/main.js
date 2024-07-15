cea2dfe0a280712ad8bbfdc2c1400dd4131ec913
sukumarrkv <kypa.sukumar3@gmail.com>
2024-07-15 11:29:37 +0530

    Added more notes on promises

--- asynchronous-javascript/main.js
+++ asynchronous-javascript/main.js
@@ -82,36 +82,92 @@ greetSukumar(greet);
 //How to execute a callback function based on whetherthe promise is fullfilled or rejected (Point 5 and 6)
 
 //How to create a Promise
-const promise = new Promise();
+//const promise = new Promise();
 
 //How to fullfill or reject a Promise
 //Promise takes and function as argument and accepts two functions as argument, one if resolve function and other is reject function
 //
-// const resolvePromise = new Promise((resolve, reject) -> {
-//    //When resolve is called changes the status of promise from pending to fullfilled
-//    resolve();
-// })
+const resolvePromise = new Promise((resolve, reject) => {
+   //When resolve is called changes the status of promise from pending to fullfilled
+   resolve();
+})
 
-// const rejectPromise = new Promise((resolve, reject) -> {
-//     //When reject is called changes the status of promise from pending to rejected
-//     reject();
-//  })
+const rejectPromise = new Promise((resolve, reject) => {
+    //When reject is called changes the status of promise from pending to rejected
+    reject();
+ })
 
  //But both these functions are called after a async operation
 
-//  const asyncResolve = new Promise((resolve, reject)->{
-//     setTimeout(()->{
-//         //Food truck found
-//         //change the status from pending to fullfilled
-//         resolve();//This function is called after 5 seconds
-//     }, 5000)
-//  })
+ //Resolve is the function when called changes the status of promise from pending to fullfilled.
+ const asyncResolve = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        //Food truck found
+        //change the status from pending to fullfilled
+        resolve();//This function is called after 5 seconds
+    }, 5000)
+ })
 
-//  const asyncReject = new Promise((resolve, reject)->{
-//     setTimeout(()->{
-//         //Food truck not found
-//         //change the status from pending to rejected
-//         reject();//This function is called after 5 seconds
-//     }, 5000)
-//  })
+ //Reject is the function when called changes the status of promise from pending to rejected.
+ const asyncReject = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        //Food truck not found
+        //change the status from pending to rejected
+        reject();//This function is called after 5 seconds
+    }, 5000)
+ })
 
+ //How to execute callback function based on the status change?
+ //Lets define what function needs to be called for resolve and success scenarios
+ //Success and Failures Callbacks
+ const onfulfillment = () => {
+   //resolve was called
+   console.log("Setup the table to eat Nachos");
+ }
+
+ const onRejection = () => {
+    //reject is called
+    console.log("Start cooking pasta");
+ }
+
+ //The promise obejects created using new keyword gives us two methods/functions where we pass the above created two function
+ //(As we are passing a function as argument to another function both above functions are called callback functions)
+ //promise.then();
+ //promise.catch();
+ //If the staus of promise is changes from pending to fullfilled the function passed in then() is automatically get invoked.
+ //If the staus of promise is changes from pending to rejected the function passed in catch() is automatically get invoked.
+//So in our case it looks like this
+//promise.then(onfulfillment);
+//promise.catch(onRejection);
+
+//Our code works as expected but there is a room for inprovement.
+//What if we want to pass some data while the status of promise is changed.
+//To achieve this we use resolve and reject functions. This functions accept a parameter as argument which when passed
+//get automatically available in onFullfillment and onRjection function as shown below.
+const asyncResolve1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+       //Food truck found
+        //change the status from pending to fullfilled
+        resolve("Bringing tacos");//This function is called after 5 seconds
+    }, 5000)
+});
+
+const asyncReject1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        //Food truck not found
+        //change the status from pending to rejected
+        reject("Not bringing tacos. Food truck was nor found.");//This function is called after 5 seconds
+    }, 5000);
+})
+
+const onFullfillment1 = (result) => {
+    //resolve was called
+    console.log(result);
+   console.log("Setup the table to eat Nachos");
+}
+
+const onRejection1 = (error) => {
+   //reject is called
+   console.log(error);
+    console.log("Start cooking pasta");
+}
