f165b378a4562a94791a2875b2c69f51d4e7ea3f
sukumarrkv <kypa.sukumar3@gmail.com>
2024-10-22 10:29:45 +0530

    More notes and practice todo project on arrays

--- javascript-course/scripts/11.todo-list.js
+++ javascript-course/scripts/11.todo-list.js
@@ -8,3 +8,19 @@ function addTodo(){
 
   todoInputElement.value = '';
 }
+
+function addTodoVersion2(){
+  const todoInputElement = document.querySelector('.js-todo-input2');
+  const todoInputValue = todoInputElement.value;
+  todoList.push(todoInputValue);
+
+  let finalTodoHTML='';
+  for(let i=0;i<todoList.length;i++){
+    const todoHTML = `<p>${todoList[i]}<p>`;
+    finalTodoHTML = finalTodoHTML+todoHTML;
+  }
+
+  document.querySelector('.display-todo').innerHTML = finalTodoHTML;
+
+  todoInputElement.value='';
+}
