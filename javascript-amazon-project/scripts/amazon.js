8c2b087230fabb1465a653c5a9081de91306b868
sukumarrkv <kypa.sukumar3@gmail.com>
2024-11-11 10:24:25 +0530

    Made add to cart button interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -4,32 +4,33 @@
 //2) Generate the HTML
 //3) Make it interactive
 
-const products = [{
-  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image:'images/products/intermediate-composite-basketball.jpg"',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
+// const products = [{
+//   image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+//   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//   rating: {
+//     stars: 4.5,
+//     count: 87
+//   },
+//   priceCents: 1090
+// }, {
+//   image:'images/products/intermediate-composite-basketball.jpg"',
+//   name: 'Intermediate Size Basketball',
+//   rating: {
+//     stars: 4,
+//     count: 127
+//   },
+//   priceCents: 2095
+// }, {
+//   image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//   name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//   rating: {
+//     stars: 4.5,
+//     count: 56
+//   },
+//   priceCents: 799
+// }];
 
+//we are getting this products from products.js
 let productsHtml = '';
 products.forEach((product) => {
   productsHtml += `
@@ -77,10 +78,42 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart-button"
+                  data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHtml;
+
+//Now to find which product user has added to cart we will use data-attribute from HTML
+//which uniquely identifies the each product
+//Syntax rule for adding data-attribute in hmtl
+//HTML attribute should start with data- and followed any any meaningful name (eg: see above)
+//To get the data-attribute we should use dataset method
+document.querySelectorAll('.js-add-to-cart-button').forEach((buttonElement) => {
+  buttonElement.addEventListener('click', () =>  {
+    // console.log("Added to cart");
+    // console.log(buttonElement.dataset.productName);
+    const productId = buttonElement.dataset.productId;
+    let matchedItem;
+
+    cart.forEach((item) => {
+      if(productId === item.productId){
+        matchedItem = item;
+      }
+    });
+
+    //Truty value if we have some object in matchedItem
+    if(matchedItem){
+      matchedItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      }); 
+    }
+    console.log(cart);
+  });
+});
