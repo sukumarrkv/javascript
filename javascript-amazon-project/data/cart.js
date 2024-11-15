9d561cd07442819bbf363ec34abdb937037caec7
sukumarrkv <kypa.sukumar3@gmail.com>
2024-11-15 09:55:49 +0530

    Implemented localStorage to save cart data

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,10 +1,20 @@
-export const cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+ cart = [{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
+}
+
+
+//Variables are reset when click refresh or we go to new page. We should use localStorage to persist correct data
+function saveToLocalStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
   let matchedItem;
@@ -24,6 +34,8 @@ export function addToCart(productId){
         quantity: 1
       }); 
     }
+
+    saveToLocalStorage();
 }
 
 export function removeFromCart(productIdToBeDeleted){
@@ -32,4 +44,6 @@ export function removeFromCart(productIdToBeDeleted){
       cart.splice(index,1);
     }
   });
+
+  saveToLocalStorage();
 }
