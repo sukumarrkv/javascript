cdfc9d966affc810a7446fbee37e143ce7fa729d
sukumarrkv <kypa.sukumar3@gmail.com>
2024-11-25 10:16:41 +0530

    Debugging local storage issue

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,6 +1,7 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart){
+  console.log("Inside cart initialization");
  cart = [{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
