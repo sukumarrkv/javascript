e0671dac107915764bf2dc14d700a151d4d609ba
sukumarrkv <kypa.sukumar3@gmail.com>
2024-12-04 10:31:35 +0530

    Added few more jasmine tests in moneyTest.js

--- javascript-amazon-project/tests-jasmine/moenyTest.js
+++ javascript-amazon-project/tests-jasmine/moenyTest.js
@@ -4,4 +4,16 @@ describe('Test suite: format currency', () => {
   it('Testing format currency function', () => {
     expect(formatCurrency(2095)).toEqual('20.95');
   }); 
+
+  it('Works with zero', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  }); 
+
+  it('Rounds of to nearest value', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  }); 
+
+  it('Rounds of to nearest value', () => {
+    expect(formatCurrency(2000.4)).toEqual('20.00');
+  });
 });
