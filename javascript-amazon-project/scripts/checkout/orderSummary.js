428279fb3291bf63f5bb3b92feb8146372b03718
sukumarrkv <kypa.sukumar3@gmail.com>
2024-12-18 10:15:16 +0530

    Modified product into Product Class

--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -48,7 +48,7 @@ export function renderOrderSummary() {
               ${matchedProduct.name}
             </div>
             <div class="product-price">
-              $${formatCurrency(matchedProduct.priceCents)}
+              ${matchedProduct.getPrice()}
             </div>
             <div class="product-quantity">
               <span>
