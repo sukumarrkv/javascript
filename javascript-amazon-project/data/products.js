5e532749236c88f4c5f11bdacbec818308ac17e0
sukumarrkv <kypa.sukumar3@gmail.com>
2024-12-19 10:29:13 +0530

    Implemented inheritance and polymorphism

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -35,11 +35,29 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
 
-//console.log(new Product());
 
-console.log(new Product(  {
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+    `;
+  }
+}
+
+console.log(new Clothing(  {
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   image: "images/products/athletic-cotton-socks-6-pairs.jpg",
   name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -52,8 +70,46 @@ console.log(new Product(  {
     "socks",
     "sports",
     "apparel"
-  ]
+  ],
+  type: "clothing",
+  sizeChartLink: "images/clothing-size-chart.png"
 }));
+
+//console.log(new Product());
+
+// console.log(new Product(  {
+//   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//   image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//   name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//   rating: {
+//     stars: 4.5,
+//     count: 87
+//   },
+//   priceCents: 1090,
+//   keywords: [
+//     "socks",
+//     "sports",
+//     "apparel"
+//   ]
+// }));
+
+//Built in JavaScript classes:
+
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+
+//this keyword - see video
+/*
+console.log(this); //undefined
+
+const object1 = {
+  a: 2,
+  b: this.a //this will throw error
+}
+*/
+
+
 
 export const products = [
   {
@@ -715,7 +771,10 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
-console.log(products);
+//console.log(products);
